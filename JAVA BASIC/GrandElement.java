c3f0eb101e758c3556a94a214942443e897353e3
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-09-28 20:21:59 +0100

    suit solution

--- JAVA BASIC/GrandElement.java
+++ JAVA BASIC/GrandElement.java
@@ -2,11 +2,15 @@ public class GrandElement {
     public static void main(String[] args) {
         /*Maximum dans un tableau Lire un tableau d’entiers et trouver le plus
         grand élément. */
-        int[] Numbers = new int[]{12,15,18,20,19};
-        int COMP = 0;
+        int[] Numbers = new int[]{12,15,18,20,5};
+        int COMP1 = 0;
         for(int i=0;i<Numbers.length;i++){
-            COMP = Numbers[i]-Numbers[i++];
-            // A Faire
+            if(Numbers[i]>Numbers[i++]){
+                COMP1 += Numbers[i];
+            }else{
+                COMP1 += Numbers[i++];
+            }
         }
+        System.out.println(COMP1);
     }
 }
