479b29b5b2e80c21951a53f8f7e7641a22ee3b11
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-02 22:41:08 +0100

    +Python Basic

--- PYTHON BASIC/BASIC.py
+++ PYTHON BASIC/BASIC.py
@@ -1,4 +1,20 @@
 #BASIC 
 print("Hello World") # Pour Afficher
 # Les Variable
+username = "ilyas" # var string
+age = 20 # var int
+wallet = 100.12 # var float
+isHappy = True # var Bool
+# Affichage des var 
+print(username,age) 
+print(wallet,isHappy)
+# la fonction str pour que la var age se transforme de int to string
+print("Salut "+username+" vous avez "+str(age)+" ans") 
+# recolter des valeur de l'utilisateur
+ProductName = input("Entrez le nom du produit") #pour que le user entre le nom du produit
+Price = int(input("Entrez le prix du produit")) #pour que le user entre le prix 
+#le int car la valeur entrez par user va etre string 
+TVA = (20*Price)/100 # on a transformer Pric en int pour l'inclure dans cette operation 
+Totale = Price-TVA
+print("le Nom du Produit "+ProductName+" le prix "+str(Price)+" la tva 20% "+str(TVA)+" le totale apres la tva "+str(Totale))
 
