8a8f59badba270c92ca8e94e4f8078c887ef3e46
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-03 23:21:52 +0100

    Ex4 py

---
+++ PYTHON BASIC/EX FIRST TEST/JeuDeDevinette.py
@@ -0,0 +1,12 @@
+## Exercice 5 : Jeu de devinette
+'''
+Créez un jeu où :
+- Le programme génère un nombre aléatoire entre 1 et 10
+- L'utilisateur doit deviner le nombre
+- Le programme donne des indices ("Plus" ou "Moins")
+- Affiche un message de félicitations quand c'est trouvé
+
+ASTUCE : Utilisez import random et random.randint(1, 10)
+'''
+print("--------Jeu De Devinette -----------")
+# A continue
