e6db95f5f07a251e743cb1c31e255fbe971111ea
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-05 00:14:15 +0100

    Ex10 modif py

--- PYTHON BASIC/LISTE EX/NIVEAU3AVANCÉ.py
+++ PYTHON BASIC/LISTE EX/NIVEAU3AVANCÉ.py
@@ -50,9 +50,9 @@ print(nombres_set)  # Exemple de sortie : {1, 2, 3, 4, 5}
     • Parcours la liste avec une boucle for
     • Pour chaque élément, affiche sa valeur et son type
 '''
-Liste = [42,"hello",3.14,True,"python"]
-for i in Liste:
-   print(f"{i}  {type(i)}")
+objects = [42,"hello",3.14,True,"python"]
+for object in objects:
+   print(f"{object}  {type(object)}")
 
 # for i in range(len(Liste)):
 #     print(Liste[i])
