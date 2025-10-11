fa55e0cebdc740a7599e142e04c2b7f222d72751
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-11 19:32:21 +0100

    Modif

--- PYTHON BASIC/EX Tri par Insertion/EX2_afficher_EtapeParEtape.py
+++ PYTHON BASIC/EX Tri par Insertion/EX2_afficher_EtapeParEtape.py
@@ -12,6 +12,7 @@ Exemple :
 def Tri_Par_Insertion(tab):
     for i in range(1,len(tab)):
         element = tab[i]
+        
         j = i-1
         while j>=0 and tab[j]>element:
             tab[j+1]=tab[j]
